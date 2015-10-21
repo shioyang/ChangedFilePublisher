cbaa92dd2ab9cb9f7f09d7215ab4e6a9b6dbb202
shioyang <shioyang4321@gmail.com>
2015-10-21 09:47:14 +0900

    Detect files changed by fs.watch

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 
+var dir = 'C:/temp/ChangedFilePublisher';
+
+var fs = require('fs');
 var io = require('socket.io').listen(8888);
 
 io.sockets.on('connection', function (socket) {
 	socket.emit('greeting', 'Hello!');
+
+	fs.watch(dir, function(event, filename) {
+		socket.emit('changed', "---\nDetect changed event: " + event);
+		if (filename)
+			socket.emit('changed', "Detect changed: " + filename);
+		else
+			socket.emit('changed', "Detect changed: (Filename is not provided)");
+	});
 });
 
