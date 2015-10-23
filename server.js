c76653ac0c4f672a350f56920303a8ffd906798f
shioyang <shioyang4321@gmail.com>
2015-10-23 23:07:28 +0900

    Send JSON which includes filename

--- server.js
+++ server.js
@@ -8,11 +8,14 @@ io.sockets.on('connection', function (socket) {
 	socket.emit('greeting', 'Hello!');
 
 	fs.watch(dir, function(event, filename) {
-		socket.emit('changed', "---\nDetect changed event: " + event);
-		if (filename)
-			socket.emit('changed', "Detect changed: " + filename);
-		else
-			socket.emit('changed', "Detect changed: (Filename is not provided)");
+		if (filename) {
+			var changedInfo = {
+				filenames: [ filename ]
+			};
+			socket.emit('changed', JSON.stringify(changedInfo));
+//		} else {
+//			socket.emit('changed', "Detect changed: (Filename is not provided)");
+		}
 	});
 });
 
